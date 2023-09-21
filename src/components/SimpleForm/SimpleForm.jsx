7210bba43dab57b7c5675df7c5be95660a54ad9b
RAZU-sheikh <razu351724@gmail.com>
2023-09-22 05:05:17 +0600

    amara e.targer.name.value use kora clind taka data nave

--- src/components/SimpleForm/SimpleForm.jsx
+++ src/components/SimpleForm/SimpleForm.jsx
@@ -1,14 +1,17 @@
 const SimpleForm = () => {
     const handleSubmite = e => {
         e.preventDefault();
-        console.log(e.target)
+        console.log(e.target.name.value)
+        console.log(e.target.email.value)
         console.log('from submitted')
     }
     return (
         <div>
             <form onSubmit={handleSubmite}>
-                <input type="text" name="email" />
-                <input type="email" name="email" />
+                <br />
+                <input type="text" name="name" placeholder="type your name"/>
+                <br />
+                <input type="email" name="email"  placeholder="type your email"/>
                 <br />
                 <input type="submit" value="submit" />
             </form>
