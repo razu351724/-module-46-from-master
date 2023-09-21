e850c023268e87e35ab443d14c1ddd913153fef8
RAZU-sheikh <razu351724@gmail.com>
2023-09-22 04:33:38 +0600

    jokon amra submit click kore taon website barbar relode hoy but e.preventDefault use korla barbar relode dakbana and e.target oi commponet ar vitor ja aca sov poyo ji

--- src/components/SimpleForm/SimpleForm.jsx
+++ src/components/SimpleForm/SimpleForm.jsx
@@ -1,13 +1,14 @@
 const SimpleForm = () => {
     const handleSubmite = e => {
         e.preventDefault();
-        
+        console.log(e.target)
         console.log('from submitted')
     }
     return (
         <div>
             <form onSubmit={handleSubmite}>
-                <input type="text" />
+                <input type="text" name="email" />
+                <input type="email" name="email" />
                 <br />
                 <input type="submit" value="submit" />
             </form>
